0088b2e354d87d2ffeb0f357d0f8095fe551a968
shekh basir <shekhbasir9821273349@gmail.com>
2025-12-27 23:48:33 +0530

    biggest fear of the code

--- bas.py
+++ bas.py
@@ -1,1 +1,94 @@
-print("i am going to lern generatiuve AI")
+# print("i am the best genAi Developer");
+
+# name="sheikh basir"
+# age=23
+# class1="betch"
+
+# print(f"i am {name} and i am {age} years old and i studey {class1}")
+
+# and i a going to writing the cpde 
+
+# name="basir"
+# print(name[2])
+
+# num1="23"
+# num1=int(num1)
+# num2=7
+# total=num1+num2
+# print(total)
+#there is diffrent types of the data is theree 
+
+# num1=12
+# num2=23.3
+# naam="sheikh basir"
+# list=[1,2,3,45,6]
+# tuple1=(12,34,45,56)
+# set1={23,34,45,23,33,23,34}
+
+# print(set1)
+# list.append(786)
+# print(list)
+# list.sort()
+# print(list)
+
+# working withthe help of the operatop
+
+# num1=2
+# num2=3
+
+# if(num1  num2):{
+#   print("yess it is equal")
+# }
+# print("not match")
+
+
+# now i am going to tacking the input 
+
+# name=input("enter the name ")
+# print(f"the name of the language {name}")
+
+# number1=int(input("enter the role number "))
+# print(f"the number of the student {number1}")
+
+# for i in 10 :
+#   print(i)
+
+# from the text to many element it will going to used 
+
+# string1="sheikh basir "
+# string2="nepal"
+
+# print("this is the normal string ",string1)
+
+# concat=string1 +""+string2
+# print(concat);
+
+# replaceval=string2.replace("nepal","pakistan");
+# print(replaceval);
+
+# print(len(string1))
+
+
+# stringdata="i love nepal from the bado badi";
+# for i  in range(0,len(stringdata)-1):
+#   print(i)
+
+
+# here i am going to working with 
+number1=23
+number2=54
+
+# if number1 == number2:
+#   print("i am basir")
+# else:print("no i am the father of the all developer ")
+
+# working with multiole condo
+
+if number1 == number2 :
+  print("this nummber is equal  ")
+elif number1 <number2 :
+  print("number1 is less than two")
+elif number1 != number2 :
+  print("the numebr is greater then number 1")
+else:
+  print("nothing is match ")
