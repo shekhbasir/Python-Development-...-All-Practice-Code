654d50b0b70bf85d7ecf788f7f0a9156df732aa8
shekh basir <shekhbasir9821273349@gmail.com>
2025-12-28 13:55:06 +0530

    this is simple

--- bas.py
+++ bas.py
@@ -1,6 +1,19 @@
-list=[1,2,3,4,5,6]
-i=0
-while i!=7:
-  print("i am scam")
-i=i+1
-  
+# all file related task 
+
+# hamarfile = open("practice/las.txt", "r")
+# readblefile = hamarfile.read()
+# print(readblefile)
+# hamarfile.close()
+
+# now i am going to writing the conntent 
+
+hamarfile1=open("practice/las.txt","a")
+hamarfile1.write("in my life pinkey will definatly one day ")
+
+hamarfile1.close()
+
+
+hamarfile = open("practice/las.txt", "r")
+readblefile = hamarfile.read()
+print(readblefile)
+hamarfile.close()
